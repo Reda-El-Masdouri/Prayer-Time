5c1c2b078289647dc294678146e1d854bf8fef2f
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-12-24 21:38:35 +0100

    add moment lib

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,7 +6,7 @@ export const fetchAsyncCountries =  async () => {
     return response.data.data.filter((country: Country) => country.iso2 !== "EH");
 };
 
-export const fetchAsyncPrayersTimesOfCity = async (country:string, city: string) => {
+export const fetchAsyncPrayersTimesOfCity = async (country:string, city: string) => {    
     const response = await axios.get(`http://api.aladhan.com/v1/timingsByCity/18-12-2023?city=${city}&country=${country}&method=8`);    
     return response.data.data.timings;
 }
