4929b3ce6f971a60367789ae0fb9fd179a4d5eb1
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2024-04-01 17:20:23 +0200

    ⚡️ implement useWindowWidth

--- src/components/PrayersCards/PrayersCards.tsx
+++ src/components/PrayersCards/PrayersCards.tsx
@@ -4,11 +4,13 @@ import PrayersCardsProps from './PrayersCardsProps'
 import PrayerCard from '../PrayerCard/PrayerCard'
 import PrayerCardProps from '../PrayerCard/PrayerCardProps'
 import './PrayersCards.scss'
+import { useWindowWidth } from '@react-hook/window-size'
 
 export default function PrayersCards({ prayers }: PrayersCardsProps) {
+  const width = useWindowWidth()
   return (
     <div className="container-cards">
-      <Stack direction="row" spacing={4}>
+      <Stack direction={width >= 850 ? 'row' : 'column'} spacing={4}>
         {prayers.map((prayer: PrayerCardProps, index: number) => {
           return (
             <PrayerCard
@@ -16,6 +18,7 @@ export default function PrayersCards({ prayers }: PrayersCardsProps) {
               image={prayer.image}
               prayerName={prayer.prayerName}
               time={prayer.time}
+              myWidth={width}
             />
           )
         })}
